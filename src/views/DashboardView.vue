b8e5c98eb3d1ba2f54f77e40d26973841bf35ce1
Muhammad Faran Ayyaz <faranayyaz@gmail.com>
2023-09-20 00:27:30 +0300

    Added the vue router and created Dashboard and About views

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'DashboardView',
+}
+</script>
+
+<style scoped>
+/* Dashboard styles go here */
+</style>
